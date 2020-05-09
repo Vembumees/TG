8aab88e8115f18f2e0e84e0b04abf494e4e253a7
Peeter Pärk <Gregorythemat@gmail.com>
2020-05-09 18:46:48 +0300

    backup before starting converting to controller

---
+++ Source/TG/Controllers/ExploreController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ExploreController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TG_API AExploreController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
