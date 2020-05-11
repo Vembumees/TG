0556961748bd7b8bf9050e730d16ee35505984a8
Peeter Pärk <Gregorythemat@gmail.com>
2020-05-11 10:30:28 +0300

    mainmenuclasses and blueprints organizing

---
+++ Source/TG/UI/TGHUD.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TGHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TG_API ATGHUD : public AHUD
+{
+	GENERATED_BODY()
+
+	/*should hold references to all the widgets we have in game and manages the Menu statemachine*/
+	
+};
