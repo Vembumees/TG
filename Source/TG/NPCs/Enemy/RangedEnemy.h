76733be808b8ea2e2a4d86656ba76c95f3892634
Peeter Pärk <Gregorythemat@gmail.com>
2020-05-21 23:29:15 +0300

    quick backup

---
+++ Source/TG/NPCs/Enemy/RangedEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TG/NPCs/Enemy/BaseEnemy.h"
+#include "RangedEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TG_API ARangedEnemy : public ABaseEnemy
+{
+	GENERATED_BODY()
+	
+};
